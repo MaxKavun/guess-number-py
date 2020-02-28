b97aad3c136567f3877a5db759ce72763ab80b11
Max Kavun <Maksim_Kavun@epam.com>
2020-02-28 12:35:46 +0300

    Add hint + readme

--- main.py
+++ main.py
@@ -6,18 +6,25 @@ class NumberGuesser():
     def __init__(self, startNum, finishNum):
         self.startNum = startNum
         self.finishNum = finishNum
+        self.randNum = random.randint(self.startNum,self.finishNum)
 
-    @property
-    def randNum(self):
-        return random.randint(self.startNum,self.finishNum)
+    def checker(self, inputNum):
+        result = True if myNumber == self.randNum else False
+        hint = '\n' + self.hint(inputNum)
+        msg = "You lucky" if result == True else "You number is not the same as a random. Use from 1 to 10" + hint
+        return msg
+
+    def hint(self, inputNum):
+        msg = "Your number was higher" if inputNum > self.randNum else "Your number was lower"
+        return msg
 
 try:
     myNumber = int(sys.argv[1])
     myNewNumber = NumberGuesser(1,10)
-    print("You lucky" if myNumber == myNewNumber.randNum else "You number is not the same as a random. Use from 1 to 10")
+    print(myNewNumber.checker(myNumber))
 except IndexError:
     print("You didn't pass argument")
 except ValueError:
     print("You passed not integer, please use integer instead of other types")
 except Exception as e:
-    print("You did something wrong " + e)
+    print("You did something wrong\n" + str(e))
